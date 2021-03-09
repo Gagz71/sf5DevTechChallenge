612854dbc74719a0fb010a300ee1f21f540dbf6c
Doun Douns <sangodoun@protonmail.com>
2021-03-09 20:21:24 +0100

    affichage des membres de l'équipage enregistrés en BDD

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -24,25 +24,28 @@ class MainController extends AbstractController
      */
     public function index(Request $request): Response
     {
-    	$member = new Member();
+    	$newMember = new Member();
     	//Création du formulaire d'ajout des membres de l'équipage
-    	$form = $this->createForm(AddMembersType::class, $member);
+    	$form = $this->createForm(AddMembersType::class, $newMember);
     	$form->handleRequest($request);
     	
     	//Si le formulaire est soumis et valide
     	if($form->isSubmitted() && $form->isValid()){
     		//Hydratation du nouveau membre
-    		$member->setRegisterDate(new  \DateTime());
+    		$newMember->setRegisterDate(new  \DateTime());
     		//enregistrement en BDD
-	        $this->entityManager->persist($member);
+	        $this->entityManager->persist($newMember);
 	        $this->entityManager->flush();
 	        
 	        //Création d'une notification de succès
 	        $this->addFlash('success', 'Le nouveau membre de votre équipage a bien été enregistré !');
         }
+    	
+    	$members = $this->entityManager->getRepository(Member::class)->findAll();
 
         return $this->render('main/index.html.twig', [
-        	'form' => $form->createView()
+        	'form' => $form->createView(),
+	        'members' => $members
         ]);
     }
 }
